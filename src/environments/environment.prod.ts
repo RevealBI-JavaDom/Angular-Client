fb75643e3c20941b485bcdcb216bf1e6a847b4bf
Jason Beres <jasonberes@gmail.com>
2024-11-17 19:09:11 -0500

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  BASE_URL: 'https://acmeanalyticsserver.azurewebsites.net',
+  IMAGE_URL: 'https://acmeanalyticsserver.azurewebsites.net/images/svg/',
+  IMAGE_URL_BASE: 'https://acmeanalyticsserver.azurewebsites.net/images/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  BASE_URL: 'http://localhost:5111',
+  IMAGE_URL: 'https://acmeanalyticsserver.azurewebsites.net/images/png/',
+  IMAGE_URL_BASE: 'https://acmeanalyticsserver.azurewebsites.net/images/'
+};
